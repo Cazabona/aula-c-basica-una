d2433ef581d23c329035d96d39d39d9a8be58dfc
Isadora <isadoracazabonna15@gmail.com>
2024-08-29 16:02:22 -0300

    Update exercicio8.c

--- exercicio8.c
+++ exercicio8.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 
-int soma(int a, int b) {
-    return a + b;
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
-int main()
-{
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
-    int resultado = soma(5, 3);
-    printf("Resultado da soma: %d\n", resultado);
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array original:\n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Array ordenado:\n");
+    printArray(arr, n);
 
     return 0;
 }
