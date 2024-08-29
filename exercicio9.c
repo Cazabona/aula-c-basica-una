22a86354fd0126fadaba675ffe9d17301f186018
Isadora <isadoracazabonna15@gmail.com>
2024-08-29 16:04:15 -0300

    Update exercicio9.c

--- exercicio9.c
+++ exercicio9.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-void saudacao() {
-    printf("Bem-vindo ao programa!\n");
-}
-
 int main() {
-    saudacao();
+    float celsius, fahrenheit;
+
+    printf("Digite a temperatura em graus Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("A temperatura em Fahrenheit: %.2f°F\n", fahrenheit);
 
     return 0;
 }
