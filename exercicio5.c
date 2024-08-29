eeeb761510c86afb69338663d2c9c2f709b039ab
Isadora <isadoracazabonna15@gmail.com>
2024-08-29 15:55:01 -0300

    Update exercicio5.c

--- exercicio5.c
+++ exercicio5.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
+    char str[10000], temp;
+    int i, j;
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Iteração %d\n", i);
+    printf("Digite uma string: ");
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    int length = strlen(str);
+
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
     }
+
+    printf("String invertida: %s\n", str);
 
     return 0;
 }
